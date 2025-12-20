224c2a30dde28f84246a164a9b6420cf37b6e185
MartinVerniere <mn.verniere@gmail.com>
2025-12-20 11:17:50 -0300

    Added playwrigth testing in frontend

---
+++ blog-list-app/tests/e2e-tests.spec.js
@@ -0,0 +1,33 @@
+import { test, describe, expect } from '@playwright/test'
+
+describe('Blog-list', () => {
+	test.beforeEach(async ({ request, page }) => {
+		await request.post('http://localhost:3001/api/testing/reset')
+
+		await request.post('http://localhost:3001/api/users', {
+			data: {
+				username: 'mluukkai',
+				name: 'Matti Luukkainen',
+				password: 'salainen'
+			}
+		})
+
+		await page.goto('/')
+	})
+
+	test('front page can be opened', async ({ page }) => {
+		await expect(page.getByText('blogs')).toBeVisible()
+		await expect(page.getByText('login')).toBeVisible()
+	})
+
+	test('can login properly', async ({ page }) => {
+		await page.getByTestId('show-login-form').click()
+		await page.getByTestId('username').fill('mluukkai')
+		await page.getByTestId('password').fill('salainen')
+		await page.getByTestId('login-button').click()
+
+		await expect(
+			page.getByText('Matti Luukkainen logged in')
+		).toBeVisible()
+	})
+})
